85228ace97f6f35615518f87e95b33dc95013dce
Kim-Ji-Yeong <Kim-Ji-Yeong@users.noreply.github.com>
2024-10-04 13:18:53 +0000

    deploy: b965150bd37fd4fad132b9d4967c7ef98e272c79

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728006559',resources: [
+    cacheName: 'chirpy-1728047932',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
